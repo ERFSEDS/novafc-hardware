76cb8803883b3708bc54685e9e0065c97bc52413
Jack-Fredward <johnathan.prideaux@gmail.com>
2021-10-12 21:17:06 -0400

    initial setup of main for the board

--- Software/nova-software/data-acquisition/src/lib.rs
+++ Software/nova-software/data-acquisition/src/lib.rs
@@ -1,8 +1,8 @@
 #![no_std]
 extern crate ms5611_spi as ms5611;
-extern crate embedded_hal as hal;
 
 use ms5611::{Ms5611, Oversampling};
+use embedded_hal as hal;
 
 //gets the most recent acceleration value from the imu
 pub fn get_acceleration() {
@@ -14,19 +14,27 @@ pub fn get_high_g_acceleration() {
 
 //gets the most recent pressure value from the barometer
 pub fn get_pressure() -> i32 {
-    13
+    let sample = get_sample();
+    sample.pressure
 }
 
 //gets the most recent temperature value from the barometer
 pub fn get_temperature() -> i32 {
-    13
+    let sample = get_sample();
+    sample.temperature
 }
 
 //connects and reads from sensor
 fn get_sample() -> Sample {
-    let sample = Sample {
+    //ms5611: Ms5611
+    
+    // let sample = ms5611
+    //     .get_second_order_sample(Oversampling::OS_2048, &mut delay_source)
+    //     .unwrap();
+    // sample
+    let sample = Sample{
         pressure: 13,
-        temperature: 13,
+        temperature: 23,
     };
     sample
 }
--- Software/nova-software/main/src/main.rs
+++ Software/nova-software/main/src/main.rs
@@ -1,13 +1,120 @@
-use state_machine::StateMachine;
-use data_acquisition as da;
+// use state_machine::StateMachine;
+// use data_acquisition as da;
+// // use embedded_hal as hal;
+// extern crate ms5611_spi as ms5611;
+// use ms5611::Ms5611;
+#![no_std]
+#![no_main]
 
-fn main() {
-    let state_machine = StateMachine::new();
+use core::cell::{Cell, RefCell};
+use core::ops::DerefMut;
+use cortex_m::interrupt::{free, Mutex};
+use cortex_m_rt::entry;
+use hal::{
+    gpio::{
+        gpiog::{PG13, PG14},
+        Output, PushPull,
+    },
+    interrupt,
+    prelude::*,
+    stm32,
+    timer::{Event, Timer},
+};
 
-    println!("Hello, world!");
-    da::get_acceleration();
-    da::get_high_g_acceleration();
-    println!("Pressure: {:?}", da::get_pressure());
-    println!("Temp: {:?}", da::get_temperature());
+use panic_halt as _;
+use stm32f4xx_hal as hal;
 
+static BLINKY: Mutex<Cell<BlinkState>> = Mutex::new(Cell::new(BlinkState::OnOff));
+static TIMER: Mutex<RefCell<Option<Timer<stm32::TIM2>>>> = Mutex::new(RefCell::new(None));
+static LED_GREEN: Mutex<RefCell<Option<PG13<Output<PushPull>>>>> = Mutex::new(RefCell::new(None));
+static LED_RED: Mutex<RefCell<Option<PG14<Output<PushPull>>>>> = Mutex::new(RefCell::new(None));
+
+#[derive(Clone, Copy)]
+enum BlinkState {
+    OnOff,
+    OffOn,
 }
+
+#[entry]
+fn start() -> ! {
+    let device_periphs = stm32::Peripherals::take().unwrap();
+
+    device_periphs.RCC.apb2enr.write(|w| w.syscfgen().enabled());
+
+    let rcc_periph = device_periphs.RCC.constrain();
+
+    let clocks = rcc_periph
+        .cfgr
+        .use_hse(8.mhz()) // discovery board has 8 MHz crystal for HSE
+        .hclk(100.mhz())
+        .sysclk(100.mhz())
+        .pclk1(25.mhz())
+        .pclk2(50.mhz())
+        .freeze();
+
+    let gpiog_periph = device_periphs.GPIOG.split();
+
+    let mut _led_green = gpiog_periph.pg13.into_push_pull_output();
+    _led_green.set_high().unwrap();
+
+    let mut _led_red = gpiog_periph.pg14.into_push_pull_output();
+    _led_red.set_low().unwrap();
+
+    // Create a 1s periodic interrupt from TIM2
+    let mut _timer = Timer::tim2(device_periphs.TIM2, 1.hz(), clocks);
+
+    _timer.listen(Event::TimeOut);
+    _timer.clear_interrupt(Event::TimeOut);
+
+    free(|cs| {
+        TIMER.borrow(cs).replace(Some(_timer));
+        LED_GREEN.borrow(cs).replace(Some(_led_green));
+        LED_RED.borrow(cs).replace(Some(_led_red));
+    });
+
+    // Enable interrupt
+    stm32::NVIC::unpend(hal::stm32::Interrupt::TIM2);
+    unsafe { stm32::NVIC::unmask(hal::stm32::Interrupt::TIM2) };
+
+    loop {
+        // The main thread can now go to sleep.
+        // WFI (wait for interrupt) puts the core in sleep until an interrupt occurs.
+        cortex_m::asm::wfi();
+    }
+}
+
+#[interrupt]
+fn TIM2() {
+    free(|cs| {
+        if let (Some(ref mut _timer), Some(ref mut _led_green), Some(ref mut _led_red)) = (
+            TIMER.borrow(cs).borrow_mut().deref_mut(),
+            LED_GREEN.borrow(cs).borrow_mut().deref_mut(),
+            LED_RED.borrow(cs).borrow_mut().deref_mut(),
+        ) {
+            _timer.clear_interrupt(Event::TimeOut);
+            match BLINKY.borrow(cs).get() {
+                BlinkState::OnOff => {
+                    BLINKY.borrow(cs).replace(BlinkState::OffOn);
+                    _led_green.set_low().unwrap();
+                    _led_red.set_high().unwrap();
+                }
+                BlinkState::OffOn => {
+                    BLINKY.borrow(cs).replace(BlinkState::OnOff);
+                    _led_green.set_high().unwrap();
+                    _led_red.set_low().unwrap();
+                }
+            }
+        }
+    });
+}
+    //barometer set up
+
+    // let mut ms5611 = Ms5611::new(spi, ncs, &mut delay_source).unwrap();
+
+    // println!("Hello, world!");
+    // da::get_acceleration();
+    // da::get_high_g_acceleration();
+    // println!("Pressure: {:?}", da::get_pressure());
+    // println!("Temp: {:?}", da::get_temperature());
+
+// }
