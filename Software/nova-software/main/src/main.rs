3cc9495a5e2389057efb4027005c442896341994
Jack-Fredward <johnathan.prideaux@gmail.com>
2021-11-16 21:45:25 -0500

    needs a comment

--- Software/nova-software/main/src/main.rs
+++ Software/nova-software/main/src/main.rs
@@ -1,12 +1,14 @@
 // use state_machine::StateMachine;
 // use data_acquisition as da;
-// // use embedded_hal as hal;
+
 
 #![no_std]
 #![no_main]
 extern crate ms5611_spi as ms5611;
 use ms5611::{Ms5611, Oversampling};
 use bmi088;
+
+extern crate shared_bus as sb;
 
 // use core::cell::{Cell, RefCell};
 // use core::ops::DerefMut;
@@ -43,17 +45,31 @@ fn start() -> ! {
     let bar_pin = gpioc.pc5.into_push_pull_output();
     let gyro_accel_pin = gpiob.pb0.into_push_pull_output();
     let gyro_pin = gpiob.pb1.into_push_pull_output();
-    let mut success_led = gpiob.pb15.into_push_pull_output();
-    let mut hi_temp_light = gpiob.pb14.into_push_pull_output();
+    let mut red_led = gpioc.pc6.into_push_pull_output();
+    let mut green_led = gpiob.pb15.into_push_pull_output();
+    let mut blue_led = gpiob.pb14.into_push_pull_output();
+    let mut bright_led = gpiob.pb3.into_push_pull_output();
     // success_led.set_high();
-    success_led.set_low();
-    hi_temp_light.set_low();
+    red_led.set_high(); //high is off low is on
+    green_led.set_high(); //high is off low is on
+    blue_led.set_high(); //high is off low is on
+    bright_led.set_low(); //low is off high is on for this one
     
 // sck pa5 miso pa6 mosi pa7 
     let sck = gpioa.pa5.into_alternate();
     let miso = gpioa.pa6.into_alternate();
     let mosi = gpioa.pa7.into_alternate();
 
+    // let spi = Spi::new(
+    //     dp.SPI1,
+    //     (sck, miso, mosi),  
+    //     Mode {
+    //         polarity: Polarity::IdleLow,
+    //         phase: Phase::CaptureOnFirstTransition,
+    //     }, 
+    //     800.khz(), 
+    //     clocks
+    // );
     let spi = Spi::new(
         dp.SPI1,
         (sck, miso, mosi),  
@@ -65,38 +81,57 @@ fn start() -> ! {
         clocks
     );
 
+    let bus = sb::BusManagerSimple::new(spi);
 
 
-    let mut ms5611 = Ms5611::new(spi, bar_pin, &mut delay).unwrap();
+    // let mut ms5611 = Ms5611::new(spi, bar_pin, &mut delay).unwrap();
+    let mut ms5611 = Ms5611::new(bus.acquire_spi(), bar_pin, &mut delay).unwrap();
 
-    let mut bmi088_accel = bmi088::Builder::new_accel_spi(spi, gyro_accel_pin);
+    // let mut bmi088_accel = bmi088::Builder::new_accel_spi(spi, gyro_accel_pin);
+    let mut bmi088_accel = bmi088::Builder::new_accel_spi(bus.acquire_spi(), gyro_accel_pin);
     bmi088_accel.setup(&mut delay).unwrap();
 
-    let mut bmi088_gyro = bmi088::Builder::new_gyro_spi(spi, gyro_pin);
+    // let mut bmi088_gyro = bmi088::Builder::new_gyro_spi(spi, gyro_pin);
+    let mut bmi088_gyro = bmi088::Builder::new_gyro_spi(bus.acquire_spi(), gyro_pin);
     bmi088_gyro.setup(&mut delay).unwrap();
-
- 
-
-
 
     loop {
         let sample = ms5611.get_second_order_sample(Oversampling::OS_2048, &mut delay).unwrap();
         let temperature = sample.temperature;
-        if temperature <= 20 {
-            success_led.set_high();
-        } else if temperature > 20 {
-            hi_temp_light.set_high();
+        if temperature/100 < 20 {
+            bright_led.set_high();
+        } else if temperature/100 == 20 {
+            green_led.set_low();
+        } else if temperature/100 > 20 {
+            blue_led.set_low();
+        } else {
+            red_led.set_low();
         }
 
-        if let Ok(gyro_sample) = bmi088_gyro.get_gyro() {
-            // hprintln!("bmi088_g: {:?}", gyro_sample);
-        }
+        delay.delay_ms(1_000u16);
+        red_led.set_high();
+        green_led.set_high();
+        blue_led.set_high();
+        bright_led.set_low();
+        delay.delay_ms(1_000u16);
 
-        if let Ok(accel_sample) = bmi088_accel.get_accel() {
-            // hprintln!("bmi088_a: {:?}", accel_sample);
-        }
-        
-        // panic!("{:?}", sample.pressure);
-        // panic!("ahhhhh");
+        // if let Ok(gyro_sample) = bmi088_gyro.get_gyro() {
+        //     green_led.set_low();
+        // } else {
+        //     red_led.set_low();
+        // }
+
+        // delay.delay_ms(1_000u16);
+        // red_led.set_high();
+        // green_led.set_high();
+        // blue_led.set_high();
+        // bright_led.set_high();
+        // delay.delay_ms(1_000u16);
+
+        // if let Ok(accel_sample) = bmi088_accel.get_accel() {
+        //     blue_led.set_low();
+        // } else {
+        //     red_led.set_high();
+        // }
     }
 }
