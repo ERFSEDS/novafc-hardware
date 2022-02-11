cba463b99a6748546cbb4aa71f02e2ee6dd4dd5a
newcomb-luke <newcomb.luke@gmail.com>
2022-02-11 09:15:43 -0500

    Changed to static_alloc

--- Software/main/src/main.rs
+++ Software/main/src/main.rs
@@ -1,47 +1,36 @@
 use control::Controls;
 use data_acquisition::DataWorkspace;
-use heapless::{pool::Pool, Vec};
+use heapless::Vec;
 use nova_software_common::{
     CheckObject, CommandObject, ObjectState, MAX_CHECKS_PER_STATE, MAX_COMMANDS_PER_STATE,
     MAX_STATES,
 };
 use state_machine::{Check, Command, State, StateMachine, StateTransition, Timeout};
 
-const STATE_POOL_SIZE: usize = core::mem::size_of::<State>() * MAX_STATES;
-const CHECK_POOL_SIZE: usize = core::mem::size_of::<Check>() * MAX_CHECKS_PER_STATE * MAX_STATES;
-const COMMAND_POOL_SIZE: usize =
-    core::mem::size_of::<Command>() * MAX_COMMANDS_PER_STATE * MAX_STATES;
+use static_alloc::Bump;
 
-static mut STATE_POOL_BYTES: [u8; STATE_POOL_SIZE] = [0u8; STATE_POOL_SIZE];
-static mut CHECK_POOL_BYTES: [u8; CHECK_POOL_SIZE] = [0u8; CHECK_POOL_SIZE];
-static mut COMMAND_POOL_BYTES: [u8; COMMAND_POOL_SIZE] = [0u8; COMMAND_POOL_SIZE];
+const STATE_SIZE: usize = core::mem::size_of::<State>() * MAX_STATES;
+const CHECK_SIZE: usize = core::mem::size_of::<Check>() * MAX_CHECKS_PER_STATE * MAX_STATES;
+const COMMAND_SIZE: usize = core::mem::size_of::<Command>() * MAX_COMMANDS_PER_STATE * MAX_STATES;
+const BUMP_SIZE: usize = STATE_SIZE + CHECK_SIZE + COMMAND_SIZE;
+
+// Our static allocator
+static A: Bump<[u8; BUMP_SIZE]> = Bump::uninit();
 
 fn main() {
-    // Create the memory pools
-    let state_pool: Pool<State> = Pool::new();
-    let check_pool: Pool<Check> = Pool::new();
-    let command_pool: Pool<Command> = Pool::new();
-
-    // SAFETY: We are the only ones who access any of the *_POOL_BYTES, and Pool takes care of concurrent
-    // access
-    unsafe {
-        state_pool.grow(&mut STATE_POOL_BYTES);
-        check_pool.grow(&mut CHECK_POOL_BYTES);
-        command_pool.grow(&mut COMMAND_POOL_BYTES);
-    }
-
     let increase_data_rate = Command::new(CommandObject::DataRate, ObjectState::Flag(true), 4.0);
-    let increase_data_rate = command_pool.alloc().unwrap().init(increase_data_rate);
+    let increase_data_rate = &A.leak_box(increase_data_rate).unwrap();
     let mut launch_commands: Vec<&Command, MAX_COMMANDS_PER_STATE> = Vec::new();
-    if let Err(_) = launch_commands.push(&increase_data_rate) {
-        panic!()
-    }
+    launch_commands
+        .push(&increase_data_rate)
+        .map_err(|_| ())
+        .unwrap();
 
     let launch = State::new(String::from("Launch"), Vec::new(), launch_commands, None);
-    let launch = &state_pool.alloc().unwrap().init(launch);
+    let launch = A.leak(launch).map_err(|_| ()).unwrap();
 
     let safe = State::new(String::from("Safe"), Vec::new(), Vec::new(), None);
-    let safe = &state_pool.alloc().unwrap().init(safe);
+    let safe = A.leak(safe).map_err(|_| ()).unwrap();
 
     let mut poweron_checks: Vec<&Check, MAX_CHECKS_PER_STATE> = Vec::new();
     let continuity_check = Check::new(
@@ -49,10 +38,12 @@ fn main() {
         nova_software_common::CheckCondition::FlagSet,
         StateTransition::Transition(launch),
     );
-    let continuity_check = check_pool.alloc().unwrap().init(continuity_check);
-    if let Err(_) = poweron_checks.push(&continuity_check) {
-        panic!();
-    }
+    let continuity_check = A.leak(continuity_check).map_err(|_| ()).unwrap();
+
+    poweron_checks
+        .push(&continuity_check)
+        .map_err(|_| ())
+        .unwrap();
 
     let poweron = State::new(
         String::from("PowerOn"),
@@ -60,7 +51,7 @@ fn main() {
         Vec::new(),
         Some(Timeout::new(3.0, StateTransition::Abort(safe))),
     );
-    let poweron = &state_pool.alloc().unwrap().init(poweron);
+    let poweron = A.leak(poweron).map_err(|_| ()).unwrap();
 
     let data_workspace = DataWorkspace::new();
 
