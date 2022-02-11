b9e4465247008d065645cc7472009fb41f118aad
HernanW4 <zhernandezwalter@gmail.com>
2022-02-10 21:02:01 -0500

    Implemented the body of the traits files.

--- Software/telemetry/src/lib.rs
+++ Software/telemetry/src/lib.rs
@@ -1,1 +1,34 @@
+trait FileInterface {
+    ///Type that needs to be specified when using the FileInterface trait.
+    ///The type must also implement the trait File
+    type FileType: File;
 
+    ///This function opens a file defined by some path
+    fn open<T: AsRef<str>>(path: T, mode: FileMode) -> Result<Self::FileType, ()>;
+
+    ///Closes the current File opened.
+    fn close(file: Self::FileType) -> Result<(), ()>;
+
+    fn flash() -> Result<Self::FileType, ()>;
+}
+
+trait File {
+    ///Writes a given buffer into File
+    fn write(buffer: &[u8]) -> Result<(), ()>;
+
+    ///Reads file by modifying a given buffer and writing into it.
+    fn read(buffer: &mut [u8]) -> Result<(), ()>;
+}
+
+pub struct FileIO;
+
+//impl FileInterface for FileIO {}
+
+pub enum FileMode {
+    ReadOnly,
+    ReadWriteAppend,
+    ReadWriteTruncate,
+    ReadWriteCreate,
+    ReadWriteCreateOrTruncate,
+    ReadWriteCreateOrAppend,
+}
